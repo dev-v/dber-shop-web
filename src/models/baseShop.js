ca6a9f7506d12902c50b5b140823cb0fda922293
dev-v <250912570@qq.com>
2018-01-21 19:45:26 +0800

    代码提交

--- src/models/baseShop.js
+++ src/models/baseShop.js
@@ -1,36 +1,6 @@
 import {shopService} from '../utils/request';
+import baseService from '../utils/baseService';
 
-export default (namespace, serviceRootPath, methodNameSuffix = '') => {
-
-  return {
-    namespace,
-    state: {},
-    reducers: {},
-    effects: {
-      * [`query${methodNameSuffix}`]({page, condition}, {call}) {
-        return (yield call(shopService.post,
-          `${serviceRootPath}/query/${page}`, condition)).response;
-      },
-      * [`queryWithoutPage${methodNameSuffix}`]({condition}, {call}) {
-        return (yield call(shopService.post, `${serviceRootPath}/query`,
-          condition)).response;
-      },
-      * [`del${methodNameSuffix}`]({id}, {call}) {
-        return (yield call(shopService.get,
-          `${serviceRootPath}/del/${id}`)).response;
-      },
-      * [`delWithCondition${methodNameSuffix}`]({condition}, {call}) {
-        return (yield call(shopService.post,
-          `${serviceRootPath}/delWithCondition`, condition)).response;
-      },
-      * [`save${methodNameSuffix}`]({data}, {call}) {
-        return (yield call(shopService.post, `${serviceRootPath}/save`,
-          data)).response;
-      },
-      * [`get${methodNameSuffix}`]({id}, {call}) {
-        return (yield call(shopService.get,
-          `${serviceRootPath}/get/${id}`)).response;
-      },
-    },
-  };
+export default (namespace, serviceRootPath, methodNameSuffix) => {
+  return baseService(shopService, namespace, serviceRootPath, methodNameSuffix);
 }
---
+++ src/utils/baseService.js
@@ -0,0 +1,51 @@
+/**
+ * 使用实例
+ */
+/*
+import basePlat from './basePlat';
+1、直接使用
+export default basePlat('fitness', 'fitness_service');
+
+2、定制
+const baseFitness = basePlat('fitness', 'fitness_service');
+export default {
+  ...baseFitness,
+  effects: {
+    ...baseFitness.effects,
+    // 下面可以是扩展或覆盖默认的effects
+  },
+};*/
+
+export default (service,namespace, serviceRootPath, methodNameSuffix = '') => {
+  return {
+    namespace,
+    state: {},
+    reducers: {},
+    effects: {
+      * [`query${methodNameSuffix}`]({page, condition}, {call}) {
+        return (yield call(service.post,
+          `${serviceRootPath}/query/${page}`, condition)).response;
+      },
+      * [`queryWithoutPage${methodNameSuffix}`]({condition}, {call}) {
+        return (yield call(service.post, `${serviceRootPath}/query`,
+          condition)).response;
+      },
+      * [`del${methodNameSuffix}`]({id}, {call}) {
+        return (yield call(service.get,
+          `${serviceRootPath}/del/${id}`)).response;
+      },
+      * [`delWithCondition${methodNameSuffix}`]({condition}, {call}) {
+        return (yield call(service.post,
+          `${serviceRootPath}/delWithCondition`, condition)).response;
+      },
+      * [`save${methodNameSuffix}`]({data}, {call}) {
+        return (yield call(service.post, `${serviceRootPath}/save`,
+          data)).response;
+      },
+      * [`get${methodNameSuffix}`]({id}, {call}) {
+        return (yield call(service.get,
+          `${serviceRootPath}/get/${id}`)).response;
+      },
+    },
+  };
+}
